bcd9b8f4848a32a2891596f4b5c13caac29451c3
simenyo <simen.90@gmail.com>
2022-08-24 17:11:07 +0200

    renames2

---
+++ oppgave0/oppgave0.js
@@ -0,0 +1,15 @@
+// Oppgave 1
+// Set the element with id "remove" to null. could also use ""?
+let rmvBtn = document.getElementById("remove-btn");
+rmvBtn.addEventListener("click", () => {
+    document.getElementById("remove").innerHTML = null;
+});
+
+// Oppgave 2
+// Oppgave 3
+// Oppgave 4
+const myList = ["item one", "item two", "item three"];
+// Oppgave 5
+// Oppgave 6
+// Oppgave 7
+// Oppgave 8
