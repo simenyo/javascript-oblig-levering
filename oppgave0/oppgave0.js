0ab3adf07cb65bcfcf2e2e867b0865850f6c5653
simenyo <simen.90@gmail.com>
2022-08-24 17:37:06 +0200

    oppgave3 done

--- oppgave0/oppgave0.js
+++ oppgave0/oppgave0.js
@@ -6,10 +6,25 @@ rmvBtn.addEventListener("click", () => {
 });
 
 // Oppgave 2
+let changeBtn = document.getElementById("change-btn");
+changeBtn.addEventListener("click", () => {
+    document.getElementById("change").innerHTML = "Simen er best";
+});
+
 // Oppgave 3
+let textInputBox = document.getElementById("input");
+textInputBox.addEventListener("keyup", () => {
+    let typedText = textInputBox.value;
+    document.getElementById("input-text").innerText = typedText;
+});
+
 // Oppgave 4
 const myList = ["item one", "item two", "item three"];
+
 // Oppgave 5
+
 // Oppgave 6
+
 // Oppgave 7
+
 // Oppgave 8
