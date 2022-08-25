38a3c38a62056472d1225ead5d0087eae0632b08
simenyo <simen.90@gmail.com>
2022-08-25 17:35:40 +0200

    oppg5

--- oppgave0/oppgave0.js
+++ oppgave0/oppgave0.js
@@ -1,20 +1,20 @@
 // Oppgave 1
 // Set the element with id "remove" to null. could also use ""?
-let rmvBtn = document.getElementById("remove-btn");
+const rmvBtn = document.getElementById("remove-btn");
 rmvBtn.addEventListener("click", () => {
     document.getElementById("remove").innerHTML = null;
 });
 
 // Oppgave 2
 // Using innerText instead of innerHtml.
-let changeBtn = document.getElementById("change-btn");
+const changeBtn = document.getElementById("change-btn");
 changeBtn.addEventListener("click", () => {
     document.getElementById("change").innerText = "Simen er best";
 });
 
 // Oppgave 3
 // Fetching what's typed into the InputBox by using .value
-let textInputBox = document.getElementById("input");
+const textInputBox = document.getElementById("input");
 textInputBox.addEventListener("keyup", () => {
     let typedText = textInputBox.value;
     document.getElementById("input-text").innerText = typedText;
@@ -24,8 +24,8 @@ textInputBox.addEventListener("keyup", () => {
 const myList = ["item one", "item two", "item three"];
 
 // fetching the relevant html entities.
-let ul = document.getElementById("ul");
-let writeListBtn = document.getElementById("write-list");
+const ul = document.getElementById("ul");
+const writeListBtn = document.getElementById("write-list");
 
 // on click fetches the first element from the array until it's empty.
 writeListBtn.addEventListener("click", () => {
@@ -37,7 +37,53 @@ writeListBtn.addEventListener("click", () => {
 });
 
 // Oppgave 5
+const textVisual = document.getElementById("placeholder");
+const tagInput = document.getElementById("select");
+const textInput = document.getElementById("text");
+const createBtn = document.getElementById("create");
+let tagInputEventTriggered = false;
 
+// const TagAndText = {
+//     tag: "",
+//     text: "",
+// };
+
+/**
+ * Eventhandler on "blur" instead of "change". If user opens dropdown but does
+ * not change the value this will not trigger the eventlistener when using "change".
+ */
+// tagInput.addEventListener("blur", () => {
+//     TagAndText.tag = tagInput.value;
+//     tagInputEventTriggered = true;
+// });
+
+// textInput.addEventListener("keyup", () => {
+//     TagAndText.text = textInput.value;
+// });
+
+createBtn.addEventListener("click", () => {
+    // if (!tagInputEventTriggered) {
+    //     TagAndText.tag = tagInput.value;
+    // }
+
+    let node = document.createElement(tagInput.value);
+    node.textContent = textInput.value;
+    if (!textVisual.childElementCount) {
+        textVisual.appendChild(node);
+    }else{
+        textVisual.removeChild(textVisual.firstChild);
+        textVisual.appendChild(node);
+    }
+
+    // node.appendChild(document.createTextNode(obj.text));
+    // console.log(!placeholder.childElementCount);
+    // if (!placeholder.childElementCount) {
+    //     placeholder.appendChild(node);
+    // }else{
+    //     placeholder.removeChild(node);
+    // }
+
+});
 
 // Oppgave 6
 
