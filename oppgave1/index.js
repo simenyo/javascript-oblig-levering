8051b61f31eeaef80b83af7d8609c413b7df0a6e
simenyo <simen.90@gmail.com>
2022-08-29 19:42:23 +0200

    oppg1 done

--- oppgave1/index.js
+++ oppgave1/index.js
@@ -1,13 +1,20 @@
-import "./styles.css";
-
 // Statisk tekst som brukes til å søke i
 const text =
   "Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.";
 
 const longestWord = () => {
   // TODO: Gjør om text til array eks. ['Baby', 'cliche']
+  const textArray = text.split(" ");
   // TODO: Velg første ord så du har noe å sammenlikne med
+  let comparisonWord = textArray[0];
   // TODO: Gå igjennom alle ordene og oppdater hvis nytt ord er lengre
+  // Ternary Operator er gøy!
+  for(let i = 1; i < textArray.length; i++) {
+    comparisonWord = (comparisonWord.length < textArray[i].length)
+      ? comparisonWord = textArray[i] 
+      : comparisonWord = comparisonWord;
+  }
+  return comparisonWord;
 };
 
 console.log(longestWord());
