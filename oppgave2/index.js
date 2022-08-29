5e0817694cfe2ce054f61aca6689535185ccfa29
simenyo <simen.90@gmail.com>
2022-08-29 23:14:30 +0200

    done

--- oppgave2/index.js
+++ oppgave2/index.js
@@ -14,6 +14,8 @@ const users = [
 
 const userUl = document.getElementById("users");
 
+// TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
+// ???? denne var allerede her?
 const createTableUI = (users) => {
   userUl.innerHTML = null;
   userUl.innerHTML += `<li><span>Id</span><span>Navn</span><span>Alder</span></li>`;
@@ -23,11 +25,31 @@ const createTableUI = (users) => {
 };
 
 // TODO: Hent HTML #id med getElementById
-// TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
+const nameSearch = document.getElementById("name");
+const ageSearch = document.getElementById("age");
+const filterBtn = document.getElementById("filter");
+
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
+function handleSearch(name) {
+  let searchResult = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
+  createTableUI(searchResult);
+};
+
 // TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
+function handleFilter(age) {
+  let ageSearchResult = users.filter(user => user.age >= age);
+  createTableUI(ageSearchResult);
+};
+
 // TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
+nameSearch.addEventListener("keyup", () => {
+  handleSearch(nameSearch.value);
+});
+
 // TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
+filterBtn.addEventListener("click", () => {
+  handleFilter(ageSearch.value);
+});
 
 const main = () => {
   createTableUI(users);
